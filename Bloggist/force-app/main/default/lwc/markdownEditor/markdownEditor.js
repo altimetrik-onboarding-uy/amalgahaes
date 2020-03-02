12be68e41ecd808639147840ee41e17ddf7ba51e
AdilsonAltimetrik <60188987+AdilsonAltimetrik@users.noreply.github.com>
2020-03-02 10:36:23 -0300

    tring something with rich text

--- Bloggist/force-app/main/default/lwc/markdownEditor/markdownEditor.js
+++ Bloggist/force-app/main/default/lwc/markdownEditor/markdownEditor.js
@@ -1,6 +1,7 @@
 import { LightningElement, track } from 'lwc';
 export default class MyComponentName extends LightningElement {
     @track richtext = "<h2>Default <s>Value</s></h2>";
+    @track richtext = "<h2 style=\"text-align: center\">Default <s>Value</s></h2>";
 
     handleChange(e) {
         this.richtext = e.detail.value;
