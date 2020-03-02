d82a4f0cf41c3ca97cc14e18073314d380a1b4fe
AdilsonAltimetrik <60188987+AdilsonAltimetrik@users.noreply.github.com>
2020-03-02 11:16:22 -0300

    trying another markdown editor

--- Bloggist/force-app/main/default/lwc/markdownEditor/markdownEditor.js
+++
@@ -1,8 +0,0 @@
-import { LightningElement, track } from 'lwc';
-export default class MyComponentName extends LightningElement {
-    @track richtext = "<h2>WRITE HERE</h2>";
-
-    handleChange(e) {
-        this.richtext = e.detail.value;
-    }
-}
