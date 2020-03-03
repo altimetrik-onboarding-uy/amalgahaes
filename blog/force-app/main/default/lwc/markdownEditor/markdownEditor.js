e9c45ddd4b5285d889be8834c0344929a9d4bd96
AdilsonAltimetrik <60188987+AdilsonAltimetrik@users.noreply.github.com>
2020-03-03 10:05:58 -0300

    trying to make rich text inside the markdown

--- blog/force-app/main/default/lwc/markdownEditor/markdownEditor.js
+++ blog/force-app/main/default/lwc/markdownEditor/markdownEditor.js
@@ -3,7 +3,15 @@ import { LightningElement, track } from 'lwc';
 export default class MarkdownEditor extends LightningElement {
     @track body;
 
+
+    @track richtext = "<h2>Default <s>Value</s></h2>";//
+
     handleBodyChange(event) {
         this.body = event.target.value;
+    }
+
+
+    handleChange(e) {//
+        this.richtext = e.detail.value;
     }
 }
