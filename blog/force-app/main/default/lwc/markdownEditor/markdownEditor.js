2685d52d3eb0740f9e0b6ced4aaf186cfc2e407a
AdilsonAltimetrik <60188987+AdilsonAltimetrik@users.noreply.github.com>
2020-03-06 08:27:00 -0300

    clean markdown to build everthing again

--- blog/force-app/main/default/lwc/markdownEditor/markdownEditor.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement, track } from 'lwc';
-
-export default class MarkdownEditor extends LightningElement {
-    @track body;
-
-    handleBodyChange(event) {
-        this.body = event.target.value;
-    }
-}
