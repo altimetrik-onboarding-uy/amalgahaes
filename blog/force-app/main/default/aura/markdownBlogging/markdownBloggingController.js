5728bb1dc31c9b5f08bc7f7117fc65067500f845
AdilsonAltimetrik <60188987+AdilsonAltimetrik@users.noreply.github.com>
2020-03-11 10:25:36 -0300

    Change on the markdown on how i call the resource

--- blog/force-app/main/default/aura/markdownBlogging/markdownBloggingController.js
+++ blog/force-app/main/default/aura/markdownBlogging/markdownBloggingController.js
@@ -1,8 +1,8 @@
 ({
-    initMarkdown: function(cmp) {
+    init: function(cmp) {
         //var myResource = $A.get('$Resource.marked') + 'marked.js';
-		cmp.set('v.myValMarkdown', '');
-        //cmp.set('v.myValMarkdown', '<p><script>alert(myResource)</script></p><p>hi!</p>');
+		cmp.set('v.myVal', '');
+        //cmp.set('v.myVal', '<p><script>alert(myResource)</script></p><p>hi!</p>');
     },
     
     handleSubmit : function (cmp, evt, hlpr) {
