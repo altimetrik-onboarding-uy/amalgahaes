cedba5726c93f3ba445d6cc17960cc17404f2b20
AdilsonAltimetrik <60188987+AdilsonAltimetrik@users.noreply.github.com>
2020-03-11 11:06:35 -0300

    Creating a controller method to preview the mark

--- blog/force-app/main/default/aura/markdownBlogging/markdownBloggingController.js
+++ blog/force-app/main/default/aura/markdownBlogging/markdownBloggingController.js
@@ -4,8 +4,21 @@
 		cmp.set('v.myVal', '');
         //cmp.set('v.myVal', '<p><script>alert(myResource)</script></p><p>hi!</p>');
     },
-    
+
+    onChangeMarkdown: function (cmp, evt, hlpr) {
+        
+        //var myColor = event.getSource().get("v.value");
+        
+        //var test;
+         var myResource = $A.get('$Resource.marked') + 'marked.js';
+        
+        evt._body.set(this.template.querySelector('div').innerHTML,myResource);
+        
+        
+    },
+  
     handleSubmit : function (cmp, evt, hlpr) {
         alert(" You submit the content ");
     }
+
 });
