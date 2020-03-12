5b59d4edb51d58309c53d53805dcce25b9a00bf2
AdilsonAltimetrik <60188987+AdilsonAltimetrik@users.noreply.github.com>
2020-03-12 15:55:45 -0300

    trying to use the markdown resource

--- blog/force-app/main/default/aura/markdownBlogging/markdownBloggingController.js
+++ blog/force-app/main/default/aura/markdownBlogging/markdownBloggingController.js
@@ -8,12 +8,19 @@
     onChangeMarkdown: function (cmp, evt, hlpr) {
         
         //var myColor = event.getSource().get("v.value");
-        //var test;
-        var myResource = $A.get('$Resource.marked') + 'marked.js';
-        //evt._body.set(this.template.querySelector('div').innerHTML,myResource.querySelector('div'));
-        cmp.set('v.myVal', myResource);
-       
         
+       // var myResource = $A.get('$Resource.marked') + 'marked.js';
+        //evt._body.set(this.template.querySelector('div').innerHTML,myResource.querySelector('div'));
+        //cmp.set('v.myVal', myResource);
+       
+
+
+        var action= cmp.get('v.myval');
+        action.setCallback(this,function(data){
+            cmp.set('v.myVal', data.getReturnValue());
+
+        })
+        $A.get('$Resource.marked') + 'marked.js';
         
     },
   
