6c449e3fd189513b086119c8c7316dd2345568b1
AdilsonAltimetrik <60188987+AdilsonAltimetrik@users.noreply.github.com>
2020-03-13 11:53:48 -0300

    change the markdown css file to use in aura comp

--- blog/force-app/main/default/aura/markdownBlogging/markdownBloggingController.js
+++ blog/force-app/main/default/aura/markdownBlogging/markdownBloggingController.js
@@ -1,11 +1,19 @@
 ({
+
+
+    
+
+
+
+
     init: function(cmp) {
-        //var myResource = $A.get('$Resource.marked') + 'marked.js';
+        var myResource = $A.get('$Resource.marked') + 'marked.js';
 		cmp.set('v.myVal', '');
         //cmp.set('v.myVal', '<p><script>alert(myResource)</script></p><p>hi!</p>');
-    },
+    }
+    //,
 
-    onChangeMarkdown: function (cmp, evt, hlpr) {
+    //onChangeMarkdown: function (cmp, evt, hlpr) {
         
         //var myColor = event.getSource().get("v.value");
         
@@ -15,17 +23,17 @@
        
 
 
-        var action= cmp.get('v.myval');
-        action.setCallback(this,function(data){
-            cmp.set('v.myVal', data.getReturnValue());
+        //var action= cmp.get('v.myval');
+       // action.setCallback(this,function(data){
+          //  cmp.set('v.myVal', data.getReturnValue());
 
-        })
-        $A.get('$Resource.marked') + 'marked.js';
+        //})
+       // $A.get('$Resource.marked') + 'marked.js';
         
-    },
+    //},
   
-    handleSubmit : function (cmp, evt, hlpr) {
-        alert(" You submit the content ");
-    }
+   // handleSubmit : function (cmp, evt, hlpr) {
+      //  alert(" You submit the content ");
+   // }
 
 });
