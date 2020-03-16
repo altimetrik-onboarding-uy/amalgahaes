eeec0b85fdc40f69bfec531f884d7bf907db366d
AdilsonAltimetrik <60188987+AdilsonAltimetrik@users.noreply.github.com>
2020-03-16 13:41:33 -0300

    changes to use the markdown library

--- blog/force-app/main/default/aura/markdownBlogging/markdownBloggingController.js
+++ blog/force-app/main/default/aura/markdownBlogging/markdownBloggingController.js
@@ -1,27 +1,8 @@
 ({
-    init: function(cmp) {
+    init: function(cmp,evt, hlpr) {
         
-		cmp.set('v.myVal',  this.formatterTextMarkdown('v.myVal'));
+		cmp.set('v.myVal',  hlpr.formatterTextMarkdown('v.myVal'));
         
-    },
-    /*init: function (component, event, helper) {
-        if (component.get('v.record') != 'undefined'){
-            component.set('v.myVal', helper.formatterTextMarkdown(component.get('v.record').Content__c));
-        }
-    },
-    handleKeyShortCut: function (component, event, helper) {
-        helper.keyShortCut(component, event);
-    },
-    
+    }
 
-    handleSaveRecord: function (component, event, helper) {
-        helper.saveContentAuto(component);
-    },
-    handleSubmitReview: function (component, event, helper) {
-        helper.updateSatusUnderReview(component);
-    },
-    handleSubmitPublish: function (component, event, helper) {
-        helper.updateSatusPublish(component);
-    }*/
-    
 });
--- blog/force-app/main/default/aura/markdownBlogging/markdownBloggingHelper.js
+++ blog/force-app/main/default/aura/markdownBlogging/markdownBloggingHelper.js
@@ -1,48 +1,5 @@
 ({
-	/*saveContentAuto: function (component) {
-		component.set('v.myVal', this.formatterTextMarkdown(component.get('v.record').Content__c));
-
-		window.setTimeout(
-			$A.getCallback(function () {
-				component.find("recordEditor").saveRecord($A.getCallback(function (saveResult) {
-					if (saveResult.state === "SUCCESS" || saveResult.state === "DRAFT") {
-						console.log("The data was Saved successfully.");
-					} else if (saveResult.state === "INCOMPLETE") {
-						console.log("User is offline, device doesn't support drafts.");
-					} else if (saveResult.state === "ERROR") {
-						console.log('Problem saving record, error: ' +
-							JSON.stringify(saveResult.error));
-					} else {
-						console.log('Unknown problem, state: ' + saveResult.state + ', error: ' + JSON.stringify(saveResult.error));
-					}
-				}));
-			}), 3000
-		);
-	},
-	
 	formatterTextMarkdown: function (mytext) {
 		return marked(mytext);
-	},
-	updateSatusUnderReview: function (component) {
-		var post = component.get("v.record");
-		post.Status__c = "Under Review";
-		component.set("v.record", post);
-		this.saveContentAuto(component);
-		this.showMessage('Under Review');
-	},
-	updateSatusPublish: function (component) {
-		var post = component.get("v.record");
-		post.Status__c = "Published";
-		component.set("v.record", post);
-		this.saveContentAuto(component);
-		this.showMessage('Published');
-	},
-	showMessage: function (status) {
-		var resultsToast = $A.get("e.force:showToast");
-		resultsToast.setParams({
-			"title": "Update",
-			"message": "The status post was updated to: " + status
-		});
-		resultsToast.fire();
-	}*/
+	}
 })
