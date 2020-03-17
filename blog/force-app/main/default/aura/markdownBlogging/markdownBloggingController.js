a91504d399f84a3f6e342fd3b9a2cbf2a7718080
AdilsonAltimetrik <60188987+AdilsonAltimetrik@users.noreply.github.com>
2020-03-17 09:41:49 -0300

    path nthe button are ready.Needtoconect to post obj

--- blog/force-app/main/default/aura/markdownBlogging/markdownBloggingController.js
+++ blog/force-app/main/default/aura/markdownBlogging/markdownBloggingController.js
@@ -1,10 +1,13 @@
 ({
     init: function(cmp,evt, hlpr) {
-      if (cmp.get('v.record') != 'undefined')
+      //if (cmp.get('v.record') != 'undefined')
       //cmp.set('v.myVal',hlpr.formatterTextMarkdown('myVal'));
+      //cmp.set('v.myVal',hlpr.formatterTextMarkdown(component.get('v.simpleRecord').Content__c));
       cmp.set('v.myVal','');
     },
-    
+    handleSaveRecord: function (component, event, helper) {
+      helper.saveContentAuto(component);
+    },
     handleSubmitReview: function (cmt, event, helper) {
       helper.updateSatusUnderReview(cmt);
     },
--- blog/force-app/main/default/aura/markdownBlogging/markdownBloggingHelper.js
+++ blog/force-app/main/default/aura/markdownBlogging/markdownBloggingHelper.js
@@ -1,4 +1,24 @@
 ({
+	saveContentAuto: function (component) {
+		component.set('v.markDownText', this.formatterTextMarkdown(component.get('v.simpleRecord').Content__c));
+
+		window.setTimeout(
+			$A.getCallback(function () {
+				component.find("recordEditor").saveRecord($A.getCallback(function (saveResult) {
+					if (saveResult.state === "SUCCESS" || saveResult.state === "DRAFT") {
+						console.log("Save completed successfully.");
+					} else if (saveResult.state === "INCOMPLETE") {
+						console.log("User is offline, device doesn't support drafts.");
+					} else if (saveResult.state === "ERROR") {
+						console.log('Problem saving record, error: ' +
+							JSON.stringify(saveResult.error));
+					} else {
+						console.log('Unknown problem, state: ' + saveResult.state + ', error: ' + JSON.stringify(saveResult.error));
+					}
+				}));
+			}), 3000
+		);
+	},
 	formatterTextMarkdown: function (mytext) {
 		//return marked(mytext);
 		return '';
