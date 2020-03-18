17bd6bc6d7c66f97a1bc835480fb02d0d91347a5
AdilsonAltimetrik <60188987+AdilsonAltimetrik@users.noreply.github.com>
2020-03-18 13:07:49 -0300

    a litle change in the controller

--- blog/force-app/main/default/aura/markdownBlogging/markdownBloggingController.js
+++ blog/force-app/main/default/aura/markdownBlogging/markdownBloggingController.js
@@ -1,9 +1,9 @@
 ({
     init: function(cmp,evt, hlpr) {
-      if (cmp.get('v.record') != 'undefined')
+      //if (cmp.get('v.record') != 'undefined')
       //cmp.set('v.myVal',hlpr.formatterTextMarkdown('myVal'));
-      cmp.set('v.myVal',hlpr.formatterTextMarkdown(component.get('v.record').Content__c));
-      //cmp.set('v.myVal','');
+      //cmp.set('v.myVal',hlpr.formatterTextMarkdown(component.get('v.record').Content__c));
+      cmp.set('v.myVal','');
     },
     handleSaveRecord: function (component, event, helper) {
       helper.saveContentAuto(component);
