38b04f2ed6a3003a110da9ff30b69acac416acb3
AdilsonAltimetrik <60188987+AdilsonAltimetrik@users.noreply.github.com>
2020-03-18 13:06:20 -0300

    some changes to te controller

--- blog/force-app/main/default/aura/markdownBlogging/markdownBloggingHelper.js
+++ blog/force-app/main/default/aura/markdownBlogging/markdownBloggingHelper.js
@@ -1,6 +1,6 @@
 ({
 	saveContentAuto: function (component) {
-		component.set('v.markDownText', this.formatterTextMarkdown(component.get('v.simpleRecord').Content__c));
+		component.set('v.myVal', this.formatterTextMarkdown(component.get('v.record').Content__c));
 
 		window.setTimeout(
 			$A.getCallback(function () {
@@ -20,29 +20,23 @@
 		);
 	},
 	formatterTextMarkdown: function (mytext) {
-		//return marked(mytext);
-		return '';
+		return marked(mytext);
+		//return '';
+
 	},
 	updateSatusUnderReview: function (component) {
 		var post = component.get("v.record");
 		post.Status__c = "Under Review";
 		component.set("v.record", post);
 		this.saveContentAuto(component);
-		this.showMessage('Under Review');
+		
 	},
 	updateSatusPublish: function (component) {
 		var post = component.get("v.record");
 		post.Status__c = "Published";
 		component.set("v.record", post);
 		this.saveContentAuto(component);
-		this.showMessage('Published');
+		
 	},
-	showMessage: function (status) {
-		var resultsToast = $A.get("e.force:showToast");
-		resultsToast.setParams({
-			"title": "Update",
-			"message": "The status post was updated to: " + status
-		});
-		resultsToast.fire();
-	}
+	
 })
