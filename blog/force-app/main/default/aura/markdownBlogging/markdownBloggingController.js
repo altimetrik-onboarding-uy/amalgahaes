d21f0a3fddb57d488d148e2ef0b1062548eb9c6e
AdilsonAltimetrik <60188987+AdilsonAltimetrik@users.noreply.github.com>
2020-03-27 09:38:25 -0300

    cant find the error

--- blog/force-app/main/default/aura/markdownBlogging/markdownBloggingController.js
+++ blog/force-app/main/default/aura/markdownBlogging/markdownBloggingController.js
@@ -1,7 +1,10 @@
 ({
     onRender: function (component, event, helper) {
+      console.log('jsdjshdjshdsd');
+      console.log(component.get('v.simpleRecord'));
+      console.log('jsdjshdjshdsz');
       if (component.get('v.simpleRecord') != 'undefined')
-      component.set('v.markDownText', helper.formatterTextMarkdown(component.get('v.simpleRecord').Content__c));
+        component.set('v.markDownText', helper.formatterTextMarkdown(component.get('v.simpleRecord').Content__c));
     },
     handleSaveRecord: function (component, event, helper) {
       helper.saveContentAuto(component);
